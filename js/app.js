d9528d31f1f0236643229d97378c47a9a0d7c7e2
carlos <carlosleonfranco13@gmail.com>
2023-03-14 19:36:59 -0500

    Leer los datos del curso que seleccionamos

--- js/app.js
+++ js/app.js
@@ -1,4 +1,36 @@
 // Variables
-const carrito = document.querySelector('#carrito');
+const carrito = document.querySelector("#carrito");
+const contenedorCarrito = document.querySelector("#lista-carrito tbody");
+const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
+const listaCursos = document.querySelector("#lista-cursos");
 
-const listaCursos = document.querySelector('#lista-cursos');
+registrarEventListeners();
+function registrarEventListeners() {
+  //Cuando agregas un curso presionando "AGREGAR AL CARRITO"
+  listaCursos.addEventListener("click", agregarCurso);
+}
+
+// Funciones
+function agregarCurso(e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains("agregar-carrito")) {
+    const cursoSeleccionado = e.target.parentElement.parentElement;
+    leerDatosCurso(cursoSeleccionado);
+  }
+}
+
+// Lee el contenido del HTML al que le dimos click y extrae la información del curso
+function leerDatosCurso(curso) {
+    console.log(curso);
+
+    // Crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('.card1'),
+        titulo: curso.querySelector('h2').textContent,
+        precio: curso.querySelector('.precios span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+    }
+
+    console.log(infoCurso)
+}
